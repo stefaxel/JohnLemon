44292508f56ed1fc099d6a17f2fc54951a8253ea
stefaxel <stefan.axelson@sky.com>
2022-09-11 16:52:29 +0100

    Movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,49 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    float horizontal;
-    float vertical;
+    private float horizontal;
+    private float vertical;
+    public float turnSpeed = 20f;
 
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
     Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    private bool hasHorizontalInput;
+    private bool hasVerticalInput;
+    private bool isWalking;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize();
+
+        hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+
+        isWalking = hasHorizontalInput || hasVerticalInput;
+
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
